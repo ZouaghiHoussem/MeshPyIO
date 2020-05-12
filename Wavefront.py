866fbde2448ecae8bded3cfea791d4622a58819d
zouaghiHoussem <houssem.zouaghi@ymail.com>
2020-05-12 03:52:57 -0400

    load // form mesh

--- Wavefront.py
+++ Wavefront.py
@@ -16,28 +16,26 @@ class WavefrontOBJ:
         self.vertices  = []                 # vertices as an Nx3 or Nx6 array (per vtx colors)
         self.normals   = []                 # normals
         self.texcoords = []                 # texture coordinates
+        # Face elements
         self.faces  = []                 # M*Nv*3 array, Nv=# of vertices, stored as vid,tid,nid (-1 for N/A)
+        self.faces_coordinates_indices   =[]
+        self.faces_norm_indices          = []
         self.mesh = None            # M*Nv*3 array, Nv=# of vertices, stored as vid,tid,nid (-1 for N/A)
-
+        self.num_vertices = 0
+        self.num_faces = 0
+        self.vertex_per_face = 0
     def to_string(self):
         return "Mesh: {}, {} vertices, {} faces, {} vertices per face".format(self.name,
-                                                                              len(self.vertices),
-                                                                              len(self.faces),
+                                                                              self.vertices.shape[0],
+                                                                              self.faces.shape[0],
                                                                               len(self.faces[0]))
 
     def set_vertices(self, new_vertices : np.ndarray):
-        self.vertices = new_vertices.tolist()
+        self.vertices = new_vertices.copy()
         self.mesh = self.export_pymesh()
 
     def export_pymesh(self):
-        verts= np.array(self.vertices)[:, :3]
-        return pymesh.form_mesh(verts, self.get_faces())
-
-    def get_faces(self):
-        return np.array(self.faces)[:, :, 0]
-
-    def get_texture_coordinates(self):
-        return np.array(self.faces)[:, :, 1]
+        return pymesh.form_mesh(self.vertices, self.faces)
 
     def save_obj(self, filename: str ):
         """Saves a WavefrontOBJ object to a file
@@ -69,6 +67,35 @@ class WavefrontOBJ:
                     vstr = vstr.replace('/X/','//').replace('/X ', ' ')
                     pstr += vstr
                 ofile.write( pstr+'\n')
+
+    @staticmethod
+    def form_mesh(*args, **keywds):
+        obj_file = WavefrontOBJ(default_mtl ="default_mtl")
+        if ("vertices" not in keywds) or ("faces" not in keywds):
+            print("Error: Could not form mesh: Vertices or faces was not provided")
+            return None
+
+        if keywds["vertices"].shape[1] not in [2, 3]:
+            print("Error: Could not form mesh: Vertices must be 2D or 3D")
+            return None
+
+        if keywds["faces"].shape[1] not in [3, 4]:
+            print("Error: Could not form mesh: faces must be tri or quad")
+            return None
+
+        obj_file.vertices = keywds["vertices"]
+        obj_file.faces = keywds["faces"]
+        obj_file.num_vertices = obj_file.vertices.shape[0]
+        obj_file.num_faces = obj_file.faces.shape[0]
+        obj_file.vertex_per_face = obj_file.faces[0].shape[0]
+
+        if "faces_coordinates_indices" in keywds:
+            obj_file.faces_coordinates_indices = keywds["faces_coordinates_indices"]
+        if "faces_norm_indices" in keywds:
+            obj_file.faces_norm_indices = keywds["faces_norm_indices"]
+
+        obj_file.mesh = obj_file.export_pymesh()
+        return obj_file
 
     @staticmethod
     def load_obj(filename: str, default_mtl='default_mtl', triangulate=False):
@@ -105,7 +132,17 @@ class WavefrontOBJ:
                         if toks[1] not in obj_file.mtls:
                             obj_file.mtls.append(toks[1])
                         cur_mat = obj_file.mtls.index(toks[1])
+
+            obj_file.vertices = np.array(obj_file.vertices)[:, :3]
+            _faces = np.array(obj_file.faces)
+            obj_file.faces = _faces[:, :, 0]
+            obj_file.faces_coordinates_indices = _faces[:, :, 1]
+            obj_file.faces_norm_indices = _faces[:, :, 2]
+            obj_file.num_vertices = obj_file.vertices.shape[0]
+            obj_file.num_faces = obj_file.faces.shape[0]
+            obj_file.vertex_per_face = obj_file.faces[0].shape[0]
             obj_file.mesh = obj_file.export_pymesh()
+
             return obj_file
         except:
             print("Error when loading file {}".format(filename))
--- main.py
+++ main.py
@@ -3,19 +3,31 @@ import os
 import pymesh
 import numpy as np
 
-file_1 = os.path.join(os.path.expanduser("~/Documents/DATAs/Tibi/Energie/energie_aligned_male"), "frame-0000.obj")
-file_2 = os.path.join(os.path.expanduser("~/Documents/DATAs/Tibi/Male/Fitted/fitted"), "smoothed-0000.obj")
+file_1 = os.path.join(os.path.expanduser("~/Documents/DATAs/Tibi/Reconstruction/energie/energie_seq_new"), "frame-0000.obj")
+file_2 = os.path.join(os.path.expanduser("~/Documents/DATAs/Tibi/Reconstruction/male/Fitted/fitted"), "smoothed-0000.obj")
 
-obj = WavefrontOBJ.load_obj(file_1, triangulate=False)
+obj = WavefrontOBJ.load_obj(file_2, triangulate=False)
 print(obj.to_string())
 print(obj.vertices[0])
+print(obj.faces[0])
+print(obj.faces_norm_indices[0])
+print(obj.faces_coordinates_indices[0])
+print(obj.num_vertices)
+print(obj.num_faces)
+print(obj.vertex_per_face)
 
-
-print(obj.mesh.num_vertices)
-obj.set_vertices(obj.mesh.vertices)
-obj.save_obj("test/obj1.obj")
-obj_2 = pymesh.load_mesh(file_1)
+#print(obj.mesh.num_vertices)
+#obj.set_vertices(obj.mesh.vertices)
+#obj.save_obj("test/obj1.obj")
+#obj_2 = pymesh.load_mesh(file_1)
+obj_2 = WavefrontOBJ.form_mesh(vertices=obj.vertices, faces=obj.faces)
+print(obj_2.vertices[0])
 print(obj_2.faces[0])
+print(obj_2.num_vertices)
+print(obj_2.num_faces)
+print(obj_2.vertex_per_face)
+
+
 
 
 
